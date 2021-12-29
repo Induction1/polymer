a695216b2fd3f19bb27feff7ec6194e73097cab7
michael <mgao31415@gmail.com>
2021-12-29 00:14:42 -0500

    refactored data common to all functions

--- python_test/bm/test_brownian_motion.py
+++ python_test/bm/test_brownian_motion.py
@@ -2,27 +2,34 @@ import unittest
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib import rcParams
-from matplotlib.ticker import PercentFormatter, FuncFormatter
+from matplotlib.ticker import FuncFormatter
 
 
 class MyTestCase(unittest.TestCase):
+    def get_bm_data(self, dimensions=100, steps=100):
+        """
+
+        :param dimensions: number of realizations of bm
+        :param steps: number of steps in each realization
+        :return:
+        """
+        T = 1.
+        times = np.linspace(0, T, steps)
+        dt = times[1] - times[0]
+
+        # Bt2 - Bt1 ~ Normal with mean 0 and variance t2-t1
+
+        dB = np.sqrt(dt) * np.random.normal(size=(steps - 1, dimensions))
+        BO = np.zeros(shape=(1, dimensions))
+        B = np.concatenate((BO, np.cumsum(dB, axis=0)), axis=0)
+        return times, B
+
     def test_bm(self):
         """
         Tests Brownian Motion
         :return:
         """
-
-        n = 100
-        T = 1.
-        d = 100
-        times = np.linspace(0, T, n)
-        dt = times[1] - times[0]
-
-        # Bt2 - Bt1 ~ Normal with mean 0 and variance t2-t1
-
-        dB = np.sqrt(dt) * np.random.normal(size=(n-1, d))
-        BO = np.zeros(shape=(1,d))
-        B = np.concatenate((BO, np.cumsum(dB, axis=0)), axis=0)
+        times, B = self.get_bm_data()
         print(times.shape)
         print(B.shape)
         plt.plot(times, B)
@@ -33,33 +40,17 @@ class MyTestCase(unittest.TestCase):
         Tests the mean square distance of a Brownian motion
         :return:
         """
-
-        n = 1000
-        T = 1.
-        d = 1000
-        times = np.linspace(0, T, n)
-        dt = times[1]-times[0]
-
-        # Bt2 - Bt1 ~ normal with mean 0 and variance t2 - t1
-        dB = np.sqrt(dt) * np.random.normal(size=(n-1,d))
-        BO = np.zeros(shape=(1,d))
-        B = np.concatenate((BO, np.cumsum(dB, axis=0)), axis=0)
+        d = 100
+        times, B = self.get_bm_data(dimensions=d)
 
         msd = np.sum(B * B / d, axis=1)
         plt.plot(times, msd)
         plt.show()
 
     def test_histograph(self):
-        n = 1000    # Number of steps
-        T = 1.      # Total time in seconds
-        d = 10000    # Total number of instances
-        times = np.linspace(0, T, n)
-        dt = times[1] - times[0]
-
-        # Bt2 - Bt1 ~ normal with mean 0 and variance t2 - t1
-        dB = np.sqrt(dt) * np.random.normal(size=(n - 1, d))
-        BO = np.zeros(shape=(1, d))
-        B = np.concatenate((BO, np.cumsum(dB, axis=0)), axis=0)
+        n = 1000
+        d = 1000
+        times, B = self.get_bm_data(dimensions=d, steps=n)
 
         cross_section = B[900, :]
 
@@ -69,13 +60,14 @@ class MyTestCase(unittest.TestCase):
             # Ignores the passed in position. This has the effect of scaling the default
             # tick locations.
 
-            s = str(100.0*y / d)
+            s = str(100.0 * y / d)
 
             # The percent symbol needs escaping in LaTeX
             if rcParams['text.usetex'] is True:
                 return s + r'$\%$'
             else:
                 return s + '%'
+
         n, bins, patches = plt.hist(cross_section, bins=100)
         plt.gca().yaxis.set_major_formatter(FuncFormatter(fmt))
         plt.show()
