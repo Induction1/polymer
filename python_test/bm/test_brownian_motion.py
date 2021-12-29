a186856e48061d46b429dd44fb10ca0b27ac7b34
michael <mgao31415@gmail.com>
2021-12-29 11:23:03 -0500

    merged three plots into one

--- python_test/bm/test_brownian_motion.py
+++ python_test/bm/test_brownian_motion.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib import rcParams
 from matplotlib.ticker import FuncFormatter
+from matplotlib.patches import Polygon
 
 
 class MyTestCase(unittest.TestCase):
@@ -15,6 +16,7 @@ class MyTestCase(unittest.TestCase):
         """
         T = 1.
         times = np.linspace(0, T, steps)
+        # print(times.shape)
         dt = times[1] - times[0]
 
         # Bt2 - Bt1 ~ Normal with mean 0 and variance t2-t1
@@ -30,10 +32,12 @@ class MyTestCase(unittest.TestCase):
         :return:
         """
         times, B = self.get_bm_data()
-        print(times.shape)
-        print(B.shape)
-        plt.plot(times, B)
-        plt.show()
+
+        return times, B
+        # print(times.shape)
+        # print(B.shape)
+        # plt.plot(times, B)
+        # plt.show()
 
     def test_msd(self):
         """
@@ -44,10 +48,12 @@ class MyTestCase(unittest.TestCase):
         times, B = self.get_bm_data(dimensions=d)
 
         msd = np.sum(B * B / d, axis=1)
-        plt.plot(times, msd)
-        plt.show()
 
-    def test_histograph(self):
+        return times, msd
+        # plt.plot(times, msd)
+        # plt.show()
+
+    def test_histogram(self):
         n = 1000
         d = 1000
         times, B = self.get_bm_data(dimensions=d, steps=n)
@@ -68,6 +74,35 @@ class MyTestCase(unittest.TestCase):
             else:
                 return s + '%'
 
-        n, bins, patches = plt.hist(cross_section, bins=100)
+        plt.hist(cross_section, bins=100)
+        plt.gca().yaxis.set_major_formatter(FuncFormatter(fmt))
+        # plt.show()
+
+    def test_combine_plots(self):
+        n = 1000
+        d = 100
+        times, B = self.get_bm_data(dimensions=d, steps=n)
+        cross_section = B[900, :]
+        msd = np.sum(B * B / d, axis=1)
+
+        def fmt(y, position):
+            # Ignores the passed in position. This has the effect of scaling the default
+            # tick locations.
+
+            s = str(100.0 * y / d)
+
+            # The percent symbol needs escaping in LaTeX
+            if rcParams['text.usetex'] is True:
+                return s + r'$\%$'
+            else:
+                return s + '%'
+
+        fig = plt.figure(figsize=(9, 6))
+        sub1 = fig.add_subplot(2, 2, (1, 2))
+        sub1.plot(times, B)
+        sub2 = fig.add_subplot(2, 2, 3)
+        sub2.plot(times, msd)
+        sub3 = fig.add_subplot(2, 2, 4)
+        sub3.hist(cross_section, bins=100)
         plt.gca().yaxis.set_major_formatter(FuncFormatter(fmt))
         plt.show()
