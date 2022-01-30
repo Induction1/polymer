3eecc16f2e04c3d913f6b8542b13751c38f6033e
michael <mgao31415@gmail.com>
2022-01-30 11:16:28 -0500

    half way impl of mutliple chains

--- python/bm/brownian_motion.py
+++ python/bm/brownian_motion.py
@@ -43,31 +43,72 @@ def calculate_f(v):
         return np.zeros(v.shape)
 
 
-def from_bond_vectors_to_padded_bead_vectors(bond_vectors):
+def from_bond_vectors_of_a_chain_to_padded_bead_vectors(bond_vectors_of_a_chain):
     """
     Given n bond_vectors which links n+1 bead, assuming bead_0 is at original, produce the
     coordinates all the n+1 beads plus the padded the before and after bead.
     The before bead is 1 unit to the left of the origin and the after bead is one unit to the
     right of bean_n_plus_1.
     As a result, the coordinates n+3 beads are returned.
-    :param bond_vectors:
+    :param bond_vectors_of_a_chain:
     :return:
     """
 
-    dim = bond_vectors.shape[0]
+    dim = bond_vectors_of_a_chain.shape[0]
 
-    bead_neg_1 = np.zeros(dim).reshape((dim, -1))
+    bead_neg_1 = np.zeros((dim, 1))
     bead_neg_1[0, 0] = -1
 
-    bead_0 = np.zeros(dim).reshape(dim, -1)
+    bead_0 = np.zeros((dim, 1))
 
-    bead_1_to_n_plus_1 = np.cumsum(bond_vectors, axis=1)
+    bead_1_to_n_plus_1 = np.cumsum(bond_vectors_of_a_chain, axis=1)
 
-    bead_n_plus_2 = np.array(bead_1_to_n_plus_1[:, -1]).reshape(dim, -1)
+    bead_n_plus_2 = np.array(bead_1_to_n_plus_1[:, -1]).reshape(dim, 1)
     bead_n_plus_2[0, 0] = bead_n_plus_2[0, 0] + 1
 
     all_beads = np.concatenate((bead_neg_1, bead_0, bead_1_to_n_plus_1, bead_n_plus_2), axis=1)
     return all_beads
+
+
+def from_bond_vectors_of_n_chains_to_padded_bead_vectors(bond_vectors_of_n_chains):
+    """
+
+    :param bond_vectors_of_n_chains: Represents the bond vectors of n chains. Each has vectors of d dimension.
+    Each vector has l length. The shape is therefore [n, d, l]
+    :return: The padded chains. Each chain is padded with a before-bead that is 1 unit to the left of bead_0 and an
+    after-bead that is 1 unit to the right of the last bead bead_-1. Overall, each chain has n+3 beads (Note, the input
+    has n vectors, which connect n+1 beads. After padding with before/after beads, the total number of beads of each
+    chain becomes n+3)
+    """
+    num_of_chains = bond_vectors_of_n_chains.shape[0]
+    dim = bond_vectors_of_n_chains.shape[1]
+
+    bead_neg_1 = np.zeros((num_of_chains, dim, 1))
+    bead_neg_1[:, 0, 0] = -1
+
+    bead_0 = np.zeros((num_of_chains, dim, 1))
+
+    bead_1_to_n_plus_1 = np.cumsum(bond_vectors_of_n_chains, axis=2)
+
+    bead_n_plus_2 = np.array(bead_1_to_n_plus_1[:, :, -1]).reshape(num_of_chains, dim, 1)
+    bead_n_plus_2[:, 0, 0] = bead_n_plus_2[:, 0, 0] + 1
+
+    all_chains = np.concatenate((bead_neg_1, bead_0, bead_1_to_n_plus_1, bead_n_plus_2), axis=2)
+    return all_chains
+
+
+def generate_3d_random_bm_vectors(n):
+    """
+    Generate 3D vectors whose lengths are normally distributed and directions are randomly pointed
+    in spherical coordinates.
+    :param n: The number of 3D vectors
+    :return:
+    """
+    random_vs = generate_random_3d_unit_vectors(n)
+    random_factor = np.random.normal(0, 1, n)
+    # Apply the random factors
+    random_bm_vs = random_factor * random_vs
+    return random_bm_vs
 
 
 def generate_3d_random_bm_vectors(n):
--- python_test/bm_test/test_3d.py
+++ python_test/bm_test/test_3d.py
@@ -4,7 +4,7 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
 
-from bm.brownian_motion import calculate_f, from_bond_vectors_to_padded_bead_vectors, \
+from bm.brownian_motion import calculate_f, from_bond_vectors_of_a_chain_to_padded_bead_vectors, \
     generate_3d_random_bm_vectors
 
 
@@ -38,7 +38,7 @@ class ThreeDimTestCase(unittest.TestCase):
         # A single bond vector (1,1,1), which means bead_0 at (0, 0, 0) and bead_1 at (1, 1, 1)
         bond_vectors = np.ones(3).reshape(3, -1)
 
-        all_n_plus_3_beads = from_bond_vectors_to_padded_bead_vectors(bond_vectors)
+        all_n_plus_3_beads = from_bond_vectors_of_a_chain_to_padded_bead_vectors(bond_vectors)
         self.assertTrue(
             np.array_equal(
                 np.array([[-1., 0., 1., 2.],
@@ -48,15 +48,15 @@ class ThreeDimTestCase(unittest.TestCase):
         )
 
     def test_force_extension_of_dna(self):
-        steps = 1000
-        delta_t = 0.01
+        steps = 25000
+        delta_t = 0.05
         n = 30
         k = 0.5
         p_magnitude = 2
 
-        spring_switch = 1
-        p_switch = 1
-        bm_switch = 1
+        spring_switch = 1  # Turn on/off (1/0) the effect of the spring
+        p_switch = 1  # Turn on/off (1/0) the effect of the force p
+        bm_switch = 1  # Turn on/off (1/0) the effect of Brownian Motion
 
         bond_vectors = generate_3d_random_bm_vectors(n)
         p = np.zeros(3 * (n + 1)).reshape(3, -1)
@@ -64,7 +64,7 @@ class ThreeDimTestCase(unittest.TestCase):
         p[0, n] = p_magnitude
 
         for i in range(steps):
-            n_plus_3_beads = from_bond_vectors_to_padded_bead_vectors(bond_vectors)
+            n_plus_3_beads = from_bond_vectors_of_a_chain_to_padded_bead_vectors(bond_vectors)
             n_plus_2_bonds = np.diff(n_plus_3_beads, axis=1)
 
             n_plus_1_bm = generate_3d_random_bm_vectors(n + 1)
