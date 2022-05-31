97bf069c6faa0b221ca0206d5619403be976cfe3
michael <mgao31415@gmail.com>
2022-05-30 23:20:05 -0400

    center of mass dynamics in force extension of dna

--- python_test/bm_test/test_3d.py
+++ python_test/bm_test/test_3d.py
@@ -116,17 +116,22 @@ class ThreeDimTestCase(unittest.TestCase):
         The result will be downsampled in a separate step so that the result can be visualized. See test_process_file.py
         for downsampling.
         """
-        f = open("data10.csv", "w")
+        f = open("data11.csv", "w")
         print_curr_time()
-        num_of_chains = 100
-        steps = 500
+        num_of_chains = 500
+        steps = 500000
         delta_t = 0.00005
 
         k = 1000
         n_array = [50]
 
         p_array = [100]
-        f.write('len,k,p,num_of_chains,steps,delta_t,time,chain_e2e_avg_distance,msd\n')
+
+
+        # After each iteration, we move the bead_0 to the origin, so that
+        per_chain_cum_origin_offset = np.zeros(3 * num_of_chains).reshape(num_of_chains, -1)
+
+        f.write('len,k,p,num_of_chains,steps,delta_t,time,chain_e2e_avg_distance,msd,chain_center_of_mass_msd\n')
 
         for p_magnitude in p_array:
             for n in n_array:
@@ -142,6 +147,8 @@ class ThreeDimTestCase(unittest.TestCase):
                 p[:, 0, 0] = -p_magnitude
                 p[:, 0, n] = p_magnitude
 
+                per_chain_center_of_mass_origin = None
+
                 for i in range(steps):
                     n_plus_3_beads = from_bond_vectors_of_n_chains_to_padded_bead_vectors(bond_vectors)
                     # TODO the below line is too wasty. We should be able to pad bond_vectors directly.
@@ -154,6 +161,22 @@ class ThreeDimTestCase(unittest.TestCase):
                                            + p_switch * delta_t * p
                                            + bm_switch * bm_factor * np.sqrt(2 * delta_t) * n_plus_1_bm
                                            )
+
+                    # accumulate the bead_0 location to track how far it has moved
+                    per_chain_center_of_mass = np.sum(beads_0_to_n, axis=2) / (n + 2)
+
+                    # Add the previous offset to the center of masses to get the absolute displacement
+                    per_chain_center_of_mass += per_chain_cum_origin_offset
+                    if per_chain_center_of_mass_origin is None:
+                        per_chain_center_of_mass_origin = np.array(per_chain_center_of_mass, copy=True)
+
+                    per_chain_center_of_mass_displacement = per_chain_center_of_mass - per_chain_center_of_mass_origin
+
+                    ensemble_chain_center_of_mass_msd = np.sum(np.sum(per_chain_center_of_mass_displacement * per_chain_center_of_mass_displacement, axis=1), axis=0) / num_of_chains
+
+                    # Calculate the new offset for the next iteration
+                    per_chain_cum_origin_offset = per_chain_cum_origin_offset + np.array(beads_0_to_n[:, :, 0], copy=True)
+
                     bond_vectors = np.diff(beads_0_to_n, axis=-1)
 
                     ensemble_chain_of_the_step = np.sum(beads_0_to_n, axis=0) / num_of_chains
@@ -164,7 +187,7 @@ class ThreeDimTestCase(unittest.TestCase):
                     square_distance_of_chains = np.sum(e2e_vectors * e2e_vectors, axis=-1)
                     msd = np.sum(square_distance_of_chains) / num_of_chains
 
-                    f.write(f'{n},{k},{p_magnitude},{num_of_chains},{steps},{delta_t},{i * delta_t},{vector_magnitude(bead_n_of_the_step - bead_0_of_the_step)},{msd}\n')
+                    f.write(f'{n},{k},{p_magnitude},{num_of_chains},{steps},{delta_t},{i * delta_t},{vector_magnitude(bead_n_of_the_step - bead_0_of_the_step)},{msd},{ensemble_chain_center_of_mass_msd}\n')
 
         f.close()
         print_curr_time()
@@ -221,11 +244,11 @@ class ThreeDimTestCase(unittest.TestCase):
         # ax = fig.gca(projection='3d')
         # # Using Python unpack syntax. Could also use array[0], array[1], array[2]
         # ax.plot(*ensemble_chain, label='DNA chain')
-        # ax.plot(*beads_0_to_n[0], label='DNA chain')
+        # ax.plot(*beads_0_to_n_plus_1[0], label='DNA chain')
         #
         # ax.legend()
         # ax.scatter(*ensemble_chain)
-        # ax.scatter(*beads_0_to_n[0])
+        # ax.scatter(*beads_0_to_n_plus_1[0])
         # plt.show()
 
     def test_vector_magnitude(self):
